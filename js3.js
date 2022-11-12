e116fa1f95d9247821bf78ce31b6d613c3bb39ea
MICHENUY <adriytkachuk162@gmail.com>
2022-11-12 18:16:36 +0200

    Initial commit

---
+++ js3.js
@@ -0,0 +1,87 @@
+// let x = +prompt("Введіть число");
+// if (x === 0){
+//     alert('Вірно');
+// }else {
+//     alert("Невірно");
+// }
+
+// let time = +prompt("введіть годину");
+// if (0 <= time && time <=15 ){
+//     alert('перша чверть');
+// } else if ( 15 < time && time <= 30 ){
+//     alert(' друга чверть');
+// }else if (30 < time && time  <= 45){
+//     alert('третя година');
+// }else if (45 < time && time<= 60){
+//     alert ('четверта чверть')
+// }else {
+//     alert( "нє невидумуй тут ")
+// }
+
+// let day = +prompt ('введіть день ');
+// if ( 1 <= day && day <= 11 ){
+//     alert(" перша декада ");
+// }else if ( 12 <= day && day <= 21 ){
+//     alert("Друга декада ");
+// }else if (22 <= day && day <= 31 ){
+//     alert("Третя декада ")
+// }else {
+//     alert(" йой а так можна")
+// }
+
+// let work = +prompt("введи номер дня");
+// switch (work){
+//     case 1:
+//         alert("Купити макарони");
+//         break;
+//     case 2:
+//         alert("Зварити макарони");
+//         break;
+//     case 3:
+//         alert("Покормити кота");
+//         break;
+//     case 4:
+//         alert("Тепер можна самому поїсти ");
+//         break;
+//     case 5:
+//         alert("Голодаємо, макарони зацвіли");
+//         break;
+//     case 6:
+//         alert("Знову покормити кота ");
+//         break;
+//     case 7:
+//         alert("Переписати хату на кота");
+//         break;
+//     default:
+//         alert("Доїсти за котом, закусити макароном ");
+// }
+
+// let a = +prompt(" Введи перше число");
+// let b = +prompt("Введи друге число");
+//
+// if( a > b) {
+//     alert("Число" + " " + a + " " +"більше");
+// } else if  ( a < b){
+//     alert("Число" + " " + b + " " +"більше");
+// } else if ( a === b ){
+//     alert(" Числа рівні");
+// } else {
+//     alert(" Введи ще раз число");
+// }
+
+let x = prompt("print");
+if(x === false  &&  'string' || "number" || NaN || undefined || null ){
+    alert(x)
+}
+else if ( x === true &&   x === default){
+    alert(x)
+}
+
+
+
+
+
+
+
+
+
